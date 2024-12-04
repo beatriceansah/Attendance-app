7d0073a1e44a2f43aa513f007a5a41bf741c3c0d
Beatrice Ansah <beatriceansah269@gmail.com>
2024-12-04 04:42:01 -0800

    first commit

--- database/migrations/2024_12_03_145229_create_attendance_table.php
+++ database/migrations/2024_12_03_145229_create_attendance_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->string("action");
+            $table->string("class");
             $table->timestamps();
 
         });
